f323c8ed777ed2ef130d7cf46f11b297293f0684
SisiTeffy <joluwatola@gmail.com>
2022-09-09 13:06:01 -0700

    positive or negative fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,13 @@
 
 /**
 
- * main - Determines if a number is positive, negative or zero.
+ * main - print if the number is positive, zero, or negative
 
  *
+
+* Description: using the main function
+
+* this program prints "Programming is positive, zero, or negative
 
  * Return: Always 0 (Success)
 
